4fd1b8b36b2b9535ed7290f449e54f9632a77caf
gekishou <gonfuentes112@gmail.com>
2025-01-14 13:29:37 -0300

    feat: add randomization to the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
 import Pokecard from "./components/Pokecards";
 import "./styles/app.css";
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    // Generate a random index
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // Swap elements at indices i and j
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 function App() {
-  const range = Array.from({ length: 151 }, (_, i) => i + 1);
+  const range = shuffleArray(Array.from({ length: 151 }, (_, i) => i + 1));
   const [memoIds, setMemoIds] = useState(new Set());
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
